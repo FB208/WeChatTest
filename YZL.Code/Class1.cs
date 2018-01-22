a72919c5e08dbb81507b86b1b56fa77218bb34ea
杨惠超 <472189065@qq.com>
2018-01-22 14:39:59 +0800

    no message

---
+++ YZL.Code/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YZL.Code
+{
+    public class Class1
+    {
+    }
+}
---
+++ YZL.Code/WeChat/AccessToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YZL.Code.WeChat
+{
+    public class AccessToken
+    {
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
